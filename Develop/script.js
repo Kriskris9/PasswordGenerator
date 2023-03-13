9ba5ad1db8722375a4194451d7a17aee4605a39a
Kristina Ahn <kristinaahn@Kristinas-iMac.local>
2023-03-12 21:05:26 -0700

    Had user confirm whether they wanted UpperCase, LowerCase, Special Characters. Also created for loop so that User can randomize the password

--- Develop/script.js
+++ Develop/script.js
@@ -1,10 +1,40 @@
 // Assignment Code
-var SpecialCharacters = " !"#$%&'()*+,-./:;<=>?@[\]^_`{|}~" 
+var SpecialCharacters = " !\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~" 
 var  UpperCaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 var LowerCaseLetters = "abcdefghijklmnopqrstuvwxyz"
 var UserChoice = ''
-var length = ''
+var length;
+
 var generateBtn = document.querySelector("#generate");
+
+function generatePassword(){ 
+  length = prompt ("Please choose a desired length.");
+  if (length < 8 || length > 120 ) { 
+    alert("Password must be between 8 to 120 characters.");
+generatePassword();
+}; 
+
+var UPC = confirm("Do you want UpperCase Letters?");
+var LRC= confirm("Do you want LowerCaseLetters?");
+var SPC = confirm( "Do you want Special Characters?");
+if (UPC) { 
+  UserChoice += UpperCaseLetters 
+}
+if (LRC){
+  UserChoice += LowerCaseLetters
+}
+if (SPC) {
+  UserChoice += SpecialCharacters
+}
+
+var Randomized = ''
+for (var count = 0; count < length; count++) {
+  var index = Math.floor(Math.random() * UserChoice.length)
+Randomized += UserChoice[index]
+}
+return Randomized;
+
+}
 
 // Write password to the #password input
 function writePassword() {
@@ -20,9 +50,4 @@ generateBtn.addEventListener("click", writePassword);
 
 
 
-function GenerateRandomPassword()
-{ var length = prompt ("Please choose a desired length.");
-  if (length < 0 || > 120 ) { alert ("Password must be between 8 to 120 characters.")};
 
-
-}
