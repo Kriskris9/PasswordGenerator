282b0ca6265d6f22ff958a68f463da4d65a2c89a
Kristina Ahn <kristinaahn@Kristinas-iMac.local>
2023-03-12 21:40:25 -0700

    created prompts for user to decide whether they want special characters, numbers, upper or lowercase numbers. Then created a function to generate a random password and alert the user with their new password.

--- Develop/script.js
+++ Develop/script.js
@@ -2,6 +2,7 @@
 var SpecialCharacters = " !\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~" 
 var  UpperCaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 var LowerCaseLetters = "abcdefghijklmnopqrstuvwxyz"
+var Numbers = "01234567890"
 var UserChoice = ''
 var length;
 
@@ -17,6 +18,10 @@ generatePassword();
 var UPC = confirm("Do you want UpperCase Letters?");
 var LRC= confirm("Do you want LowerCaseLetters?");
 var SPC = confirm( "Do you want Special Characters?");
+var Numb = confirm (" Do you want Numbers?")
+if (Numb) {
+  UserChoice += Numbers
+}
 if (UPC) { 
   UserChoice += UpperCaseLetters 
 }
@@ -31,10 +36,12 @@ var Randomized = ''
 for (var count = 0; count < length; count++) {
   var index = Math.floor(Math.random() * UserChoice.length)
 Randomized += UserChoice[index]
-}
+}23
+
 return Randomized;
 
 }
+
 
 // Write password to the #password input
 function writePassword() {
